2bd3afae65f1df23450e362e7e54dc93a2b89097
mitchdzugan <mitchdzugan@gmail.com>
2022-12-27 15:46:03 -0600

    loop codes syncing

--- syncCodes.sh
+++ syncCodes.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-git pull
-node getCodes.js
-if [[ `git status --porcelain` ]]; then
-  git add -A
-  git commit -m "codes update via script"
-  node update.js
-  git add -A
-  git commit -m "data update via codes script"
-  git push
-else
-  echo "no changes"
-fi
-
-# while :
-# do
-# 	git pull
-# 	node update.js
-# 	git add -A
-# 	git commit -m "data update via script"
-# 	git push
-# 	sleep 50m
-# done
+while :
+do
+  git pull
+  node getCodes.js
+  if [[ `git status --porcelain` ]]; then
+    git add -A
+    git commit -m "codes update via script"
+    node update.js
+    git add -A
+    git commit -m "data update via codes script"
+    git push
+  else
+    echo "no changes"
+  fi
+  sleep 1m
+done
