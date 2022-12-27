80a5ec1d828a94713a29d2eb0e89dadca0f14288
mitchdzugan <mitchdzugan@gmail.com>
2022-12-27 15:44:58 -0600

    data update via codes script

--- syncCodes.sh
+++ syncCodes.sh
@@ -6,6 +6,7 @@ if [[ `git status --porcelain` ]]; then
   git add -A
   git commit -m "codes update via script"
   node update.js
+  git add -A
   git commit -m "data update via codes script"
   git push
 else
