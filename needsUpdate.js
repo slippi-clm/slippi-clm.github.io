88331ed18362a7417a41b997cad7b208399d0934
Mitch Dzugan <mitchdzugan@gmail.com>
2023-09-11 07:06:40 -0500

    move to single script

---
+++ needsUpdate.js
@@ -0,0 +1,18 @@
+try {
+	const data = require('./data.json');
+	if (!data.updatedAt) {
+		throw "data has no updated at field";
+	}
+	const now = Date.now() / 1000;
+	const sinceLast = (now - data.updatedAt) / 60;
+	const isNotNum  = !Object.is(typeof sinceLast, 'number');
+	const isBadNum  = sinceLast < 0;
+	const isTooLong = sinceLast > 50;
+	if (isNotNum || isBadNum || isTooLong) {
+		process.exit(1);
+	}
+	process.exit();
+} catch (e) {
+	console.error(e);
+	process.exit(1);
+}
--- single.sh
+++ single.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
+rootDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+needsUpdate="$rootDir/needsUpdate.sh"
+update="$rootDir/update.sh"
+
+function doUpdate() {
+	eval $update
+	git add -A
+	git commit -m "data update via codes script"
+	git push
+}
+
+
+cd $rootDir
+
 while :
 do
-  git pull
-  node getCodes.js
-  if [[ `git status --porcelain` ]]; then
-    git add -A
-    git commit -m "codes update via script"
-    node update.js
-    git add -A
-    git commit -m "data update via codes script"
-    git push
-  else
-    echo "no changes"
-  fi
-  sleep 1m
+	git pull
+	node getCodes.js
+	echo "checking for update"
+	eval $needsUpdate
+	if [ $? -eq 0 ]; then
+		echo "checking for sync"
+		if [[ `git status --porcelain` ]]; then
+			echo "performing sync"
+			git add -A
+			git commit -m "codes update via script"
+			doUpdate
+		fi
+	else
+		echo "performing update"
+		doUpdate
+	fi
+	sleep 1m
 done
