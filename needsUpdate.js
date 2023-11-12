89bacd6281cb4c2c2cecb3eae627d71e7234fe88
Mitch Dzugan <mitchdzugan@gmail.com>
2023-11-12 13:06:58 -0600

    remove logging

--- needsUpdate.js
+++ needsUpdate.js
@@ -8,7 +8,6 @@ try {
 	const isNotNum  = !Object.is(typeof sinceLast, 'number');
 	const isBadNum  = sinceLast < 0;
 	const isTooLong = sinceLast > 50;
-	console.log({ sinceLast, isNotNum, isBadNum, isTooLong })
 	if (isNotNum || isBadNum || isTooLong) {
 		process.exit(1);
 	}
